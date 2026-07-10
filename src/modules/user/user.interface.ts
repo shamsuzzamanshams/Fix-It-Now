5c04bdd4169f1873bd188da0de713d81d10d9a51
shamsuzzamanshams <shams30200@gmail.com>
2026-07-11 00:43:11 +0600

    Support role based registration payload

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -1,7 +1,13 @@
+import { UserRole } from "../../../generated/prisma/enums";
+
 export type RegisterUserPayload = {
   name: string;
   email: string;
   password: string;
   phone?: string;
   address?: string;
+  role?: UserRole;
+  bio?: string;
+  experience?: number;
+  location?: string;
 };
