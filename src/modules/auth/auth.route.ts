b58f57aeff9ed8f871a113a56ee5a28f8c978467
shamsuzzamanshams <shams30200@gmail.com>
2026-07-11 00:43:11 +0600

    Expose auth register and me routes

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
+import { userController } from "../user/user.controller";
+import { auth } from "../../middleware/auth";
+import { UserRole } from "../../../generated/prisma/enums";
 
 const router = Router();
 
+router.post("/register", userController.registerUser);
 router.post("/login", authController.loginUser);
+router.get("/me", auth(UserRole.ADMIN, UserRole.CUSTOMER, UserRole.TECHNICIAN), userController.getMyProfile);
 
 router.post("/refresh-token",authController.refreshToken);
 
