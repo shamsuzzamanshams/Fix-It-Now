bad7ca5e2ad3fdd9db16e451459365d022e1aadc
shamsuzzamanshams <shams30200@gmail.com>
2026-07-11 00:43:12 +0600

    Update review rating workflow

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -1,13 +1,15 @@
 import { BookingStatus } from "../../../generated/prisma/enums";
 import { prisma } from "../../lib/prisma";
 import { ReviewPayload } from "./review.interface";
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
 
 const createReviewIntoDB = async (
   userId: string,
   payload: ReviewPayload
 ) => {
 
-  const booking = await prisma.booking.findUniqueOrThrow({
+  const booking = await prisma.booking.findUnique({
     where: {
       id: payload.bookingId,
     },
@@ -16,42 +18,68 @@ const createReviewIntoDB = async (
     },
   });
 
+  if (!booking) {
+    throw new AppError(httpStatus.NOT_FOUND, "Booking not found");
+  }
+
   if (booking.customerId !== userId) {
-    throw new Error("Unauthorized");
+    throw new AppError(httpStatus.FORBIDDEN, "Unauthorized");
   }
 
   if (booking.status !== BookingStatus.COMPLETED) {
-    throw new Error("Job is not completed yet");
+    throw new AppError(httpStatus.BAD_REQUEST, "Job is not completed yet");
   }
 
   if (booking.review) {
-    throw new Error("Review already submitted");
+    throw new AppError(httpStatus.CONFLICT, "Review already submitted");
   }
 
-  const review = await prisma.review.create({
-    data: {
-      bookingId: booking.id,
-      customerId: booking.customerId,
-      technicianId: booking.technicianId,
-      rating: payload.rating,
-      comment: payload.comment,
-    },
-    include: {
-      customer: {
-        omit: {
-          password: true,
-        },
+  const review = await prisma.$transaction(async (tx) => {
+    const createdReview = await tx.review.create({
+      data: {
+        bookingId: booking.id,
+        customerId: booking.customerId,
+        technicianId: booking.technicianId,
+        rating: payload.rating,
+        comment: payload.comment,
       },
-      technician: {
-        include: {
-          user: {
-            omit: {
-              password: true,
+      include: {
+        customer: {
+          omit: {
+            password: true,
+          },
+        },
+        technician: {
+          include: {
+            user: {
+              omit: {
+                password: true,
+              },
             },
           },
         },
       },
-    },
+    });
+
+    const rating = await tx.review.aggregate({
+      where: {
+        technicianId: booking.technicianId,
+      },
+      _avg: {
+        rating: true,
+      },
+    });
+
+    await tx.technicianProfile.update({
+      where: {
+        id: booking.technicianId,
+      },
+      data: {
+        averageRating: rating._avg.rating || 0,
+      },
+    });
+
+    return createdReview;
   });
 
   return review;
